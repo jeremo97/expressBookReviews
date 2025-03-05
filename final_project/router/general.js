558c9b3f4b07501a6037dd72ea126dd06ef5d169
Jerry Moore <jeremiahmoorenc@gmail.com>
2025-03-05 13:01:26 -0500

    Adds Task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,33 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Send a JSON response containing the users array, formatted with an indentation of 4 spaces for readability
+    res.send(JSON.stringify({books}, null, 4));
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  // Filter the books array to find users whose isbn matches the extracted isbn paramter
+   // let filtered_users = users.filter((user) => user.isbn === isbn);
+ // Send the filtered_users array as the response to the client
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let book = [];
+
+  Object.keys(books).forEach(a => {
+    if(books[a].author.toLowerCase() == author.toLowerCase()){
+      book.push(books[a])
+    }
+  });
+  res.send(book);
 });
 
 // Get all books based on title
